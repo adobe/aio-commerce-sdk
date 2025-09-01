9928fdfcd7963dd35a0968e53c475b4cd5c13c93
Ivan Porto <iporto@adobe.com>
2025-09-01 10:55:35 +0200

    refactor: allow only single http client per api client

--- packages-private/aio-commerce-lib-api/source/lib/api-client.ts
+++ packages-private/aio-commerce-lib-api/source/lib/api-client.ts
@@ -2,18 +2,18 @@ import type { HttpClientBase } from "./http-client-base";
 
 /** A function that takes an object of HTTP clients and returns something. */
 type ApiFunction<
-  TClients extends Record<string, HttpClientBase<unknown>>,
+  TClient extends HttpClientBase<unknown>,
   TArgs extends unknown[],
   TResult,
-> = (clients: TClients, ...args: TArgs) => TResult;
+> = (clients: TClient, ...args: TArgs) => TResult;
 
 /** A client that bounds a set of {@link ApiFunction} to their HTTP clients. */
 type ApiClientRecord<
-  TClients extends Record<string, HttpClientBase<unknown>>,
-  TFunctions extends Record<string, ApiFunction<TClients, unknown[], unknown>>,
+  TClient extends HttpClientBase<unknown>,
+  TFunctions extends Record<string, ApiFunction<TClient, unknown[], unknown>>,
 > = {
   [K in keyof TFunctions]: TFunctions[K] extends ApiFunction<
-    TClients,
+    TClient,
     infer Args,
     infer Result
   >
@@ -24,17 +24,17 @@ type ApiClientRecord<
 // biome-ignore lint/complexity/noStaticOnlyClass: For consistency with the rest of the codebase.
 export class ApiClient {
   public static create<
-    TClients extends Record<string, HttpClientBase<unknown>>,
+    TClient extends HttpClientBase<unknown>,
     // biome-ignore lint/suspicious/noExplicitAny: We can't know the type of the argument/return type.
-    TFunctions extends Record<string, ApiFunction<TClients, any[], any>>,
-  >(clients: TClients, functions: TFunctions) {
-    const wrapped = {} as ApiClientRecord<TClients, TFunctions>;
+    TFunctions extends Record<string, ApiFunction<TClient, any[], any>>,
+  >(client: TClient, functions: TFunctions) {
+    const wrapped = {} as ApiClientRecord<TClient, TFunctions>;
 
     for (const key in functions) {
       if (Object.hasOwn(functions, key)) {
         const fn = functions[key];
         // biome-ignore lint/suspicious/noExplicitAny: We can't know the type of the argument/return type.
-        wrapped[key] = ((...args: unknown[]) => fn(clients, ...args)) as any;
+        wrapped[key] = ((...args: unknown[]) => fn(client, ...args)) as any;
       }
     }
 
