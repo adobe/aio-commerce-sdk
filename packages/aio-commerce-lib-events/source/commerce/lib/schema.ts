874c8b1d4264993c893db5633a33824e425c8ca5
Ivan Porto <iporto@adobe.com>
2025-09-02 18:15:35 +0200

    fix: workspace config schema

--- packages/aio-commerce-lib-events/source/commerce/lib/schema.ts
+++ packages/aio-commerce-lib-events/source/commerce/lib/schema.ts
@@ -4,26 +4,26 @@ import { stringValueSchema } from "~/utils/schemas";
 
 /** The schema of the workspace configuration in the Developer Console. */
 export function workspaceConfigurationSchema(propertyName: string) {
-  return v.pipe(
-    // Input Format (JSON String or Object)
-    v.union([
-      v.pipe(
-        stringValueSchema(propertyName),
-        v.parseJson(
-          undefined,
-          `Expected valid JSON string for property '${propertyName}'`,
-        ),
-
-        v.record(v.string(), v.unknown()),
-        v.stringifyJson(),
+  return v.union([
+    // Input Format (Empty String, JSON String or Object)
+    v.pipe(stringValueSchema(propertyName), v.empty()),
+    v.pipe(
+      stringValueSchema(propertyName),
+      v.parseJson(
+        undefined,
+        `Expected valid JSON string for property '${propertyName}'`,
       ),
 
       v.record(v.string(), v.unknown()),
-    ]),
-    // Output Format (JSON String)
-    v.stringifyJson(
-      undefined,
-      `Expected valid JSON data for property '${propertyName}'`,
+      v.stringifyJson(),
     ),
-  );
+
+    v.pipe(
+      v.record(v.string(), v.unknown()),
+      v.stringifyJson(
+        undefined,
+        `Expected valid JSON data for property '${propertyName}'`,
+      ),
+    ),
+  ]);
 }
