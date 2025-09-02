66f0aa4cd1070098297b2a0199ce146f812280b1
Ivan Porto <iporto@adobe.com>
2025-09-02 12:03:32 +0200

    refactor: remove transformations on fields schema

--- packages/aio-commerce-lib-events/source/commerce/api/event-subscriptions/schema.ts
+++ packages/aio-commerce-lib-events/source/commerce/api/event-subscriptions/schema.ts
@@ -3,45 +3,14 @@ import * as v from "valibot";
 import { booleanValueSchema, stringValueSchema } from "~/utils/schemas";
 
 function fieldsSchema(propertyName: string) {
-  return v.union([
-    v.pipe(
-      v.literal("*"),
-      v.transform(() => [
-        {
-          name: "*",
-        },
-      ]),
-    ),
-    v.union([
-      v.pipe(
-        // @TODO: Implement converters?
-        v.array(
-          v.string(
-            `Expected only string values for the names in the property "${propertyName}"`,
-          ),
-          `Expected an array of string values for the property "${propertyName}"`,
-        ),
-        v.minLength(1, `${propertyName} array must not be empty`),
-        v.transform((fields) =>
-          fields.map((field) => {
-            const trimmedField = field.trim();
-            return {
-              name: trimmedField,
-            };
-          }),
-        ),
-      ),
-      v.array(
-        v.object({
-          name: stringValueSchema(`${propertyName}[i].name`),
-        }),
-        `Expected an array of objects with a 'name' property for the property "${propertyName}"`,
-      ),
-    ]),
-  ]);
+  return v.array(
+    v.object({
+      name: stringValueSchema(`${propertyName}[i].name`),
+    }),
+    `Expected an array of objects with a 'name' property for the property "${propertyName}"`,
+  );
 }
 
-// @TODO: Implement rules?
 export const EventSubscriptionCreateParamsSchema = v.object({
   name: stringValueSchema("name"),
 
