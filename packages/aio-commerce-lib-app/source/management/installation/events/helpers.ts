59d622cb4972be1a1e6ae26215984a14f7726d62
Joel Atherley <atherley@adobe.com>
2026-02-05 15:45:54 +0100

    chore: add docs

--- packages/aio-commerce-lib-app/source/management/installation/events/helpers.ts
+++ packages/aio-commerce-lib-app/source/management/installation/events/helpers.ts
@@ -201,6 +201,11 @@ export function configureCommerce(context: EventsExecutionContext) {
   return { commerceConfigured: true };
 }
 
+/**
+ * Creates or updates a Commerce event subscription based on existing subscriptions.
+ * @param params
+ * @param existingData
+ */
 export async function createOrUpdateSubscription(
   params: CreateSubscriptionParams,
   existingData: Map<
@@ -312,6 +317,11 @@ export async function onboardIoEvents<
   };
 }
 
+/**
+ * Onboards Commerce event subscriptions by creating or updating them based on existing subscriptions.
+ * @param params
+ * @param existingSubscriptionsData
+ */
 export async function onboardCommerceSubscriptions(
   params: OnboardCommerceEventSubscriptionParams,
   existingSubscriptionsData: CommerceEventSubscriptionManyResponse,
