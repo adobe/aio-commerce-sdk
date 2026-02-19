35a3b26139c9f1049aebafd4d2102e4f8ce3cb06
Ivan Porto <iporto@adobe.com>
2026-02-19 19:07:26 +0100

    chore: add actions entrypoint

--- packages/aio-commerce-lib-config/tsdown.config.ts
+++ packages/aio-commerce-lib-config/tsdown.config.ts
@@ -14,5 +14,9 @@ import { baseConfig } from "@aio-commerce-sdk/config-tsdown/tsdown.config.base";
 import { mergeConfig } from "tsdown";
 
 export default mergeConfig(baseConfig, {
-  entry: ["./source/index.ts", "./source/commands/index.ts"],
+  entry: [
+    "./source/index.ts",
+    "./source/actions/index.ts",
+    "./source/commands/index.ts",
+  ],
 });
