07fbd0de583162f91bad0de993e7966c8ad2a0e1
Joel Atherley <atherley@adobe.com>
2026-02-20 17:40:13 +0100

    refactor: remove unused consts

--- configs/tsdown/tsdown.config.base.js
+++ configs/tsdown/tsdown.config.base.js
@@ -10,13 +10,9 @@
  * governing permissions and limitations under the License.
  */
 
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { join } from "node:path";
 
 import { selectiveBundlePlugin } from "#plugins/selective-bundle/plugin.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const OUT_DIR = "./dist";
 const ADOBE_LICENSE_BANNER = `
