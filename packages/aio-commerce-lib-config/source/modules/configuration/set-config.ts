d12cdc13453afdcf12547f4228e66396b340faaa
Ivan Porto <iporto@adobe.com>
2026-02-27 15:40:26 +0100

    fix: pass encryption key

--- packages/aio-commerce-lib-config/source/modules/configuration/set-config.ts
+++ packages/aio-commerce-lib-config/source/modules/configuration/set-config.ts
@@ -63,6 +63,7 @@ export async function setConfiguration(
   const encryptedEntries = encryptPasswordFields(
     sanitizedEntries,
     passwordFields,
+    context.encryptionKey,
   );
 
   const existingPersisted = await configRepository.loadConfig(scopeCode);
