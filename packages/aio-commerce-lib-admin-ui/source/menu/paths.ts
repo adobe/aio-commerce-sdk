5c4a6af2c62c5e55609bb933285afff3c9eff0ee
Ivan Porto Wigner <iporto@adobe.com>
2026-06-09 16:34:57 +0200

    refactor: nested names

--- packages/aio-commerce-lib-admin-ui/source/menu/paths.ts
+++ packages/aio-commerce-lib-admin-ui/source/menu/paths.ts
@@ -11,148 +11,146 @@
  */
 
 /** Menu ID for "Catalog" in the Adobe Commerce admin */
-export const MENU_CATALOG = "catalog.catalog";
+export const MENU_CATALOG = "catalog";
 
 /** Menu ID for "Catalog > Inventory" in the Adobe Commerce admin */
 export const MENU_CATALOG_INVENTORY = "catalog.inventory";
 
 /** Menu ID for "Content" in the Adobe Commerce admin */
-export const MENU_CONTENT = "backend.content";
+export const MENU_CONTENT = "content";
 
 /** Menu ID for "Content > Content Staging" in the Adobe Commerce admin */
-export const MENU_CONTENT_CONTENT_STAGING = "backend.content.staging";
+export const MENU_CONTENT_CONTENT_STAGING = "content.staging";
 
 /** Menu ID for "Content > Design" in the Adobe Commerce admin */
-export const MENU_CONTENT_DESIGN = "backend.system.design";
+export const MENU_CONTENT_DESIGN = "content.design";
 
 /** Menu ID for "Content > Elements" in the Adobe Commerce admin */
-export const MENU_CONTENT_ELEMENTS = "backend.content.elements";
+export const MENU_CONTENT_ELEMENTS = "content.elements";
 
 /** Menu ID for "Content > Media" in the Adobe Commerce admin */
-export const MENU_CONTENT_MEDIA = "mediagalleryui.media";
+export const MENU_CONTENT_MEDIA = "content.media";
 
 /** Menu ID for "Customers" in the Adobe Commerce admin */
-export const MENU_CUSTOMERS = "customer.customer";
+export const MENU_CUSTOMERS = "customers";
 
 /** Menu ID for "Marketing" in the Adobe Commerce admin */
-export const MENU_MARKETING = "backend.marketing";
+export const MENU_MARKETING = "marketing";
 
 /** Menu ID for "Marketing > Communications" in the Adobe Commerce admin */
-export const MENU_MARKETING_COMMUNICATIONS = "backend.marketing.communications";
+export const MENU_MARKETING_COMMUNICATIONS = "marketing.communications";
 
 /** Menu ID for "Marketing > Private Sales" in the Adobe Commerce admin */
-export const MENU_MARKETING_PRIVATE_SALES = "enterprise.private.sales";
+export const MENU_MARKETING_PRIVATE_SALES = "marketing.private.sales";
 
 /** Menu ID for "Marketing > Promotions" in the Adobe Commerce admin */
-export const MENU_MARKETING_PROMOTIONS = "catalogrule.promo";
+export const MENU_MARKETING_PROMOTIONS = "marketing.promotions";
 
 /** Menu ID for "Marketing > SEO & Search" in the Adobe Commerce admin */
-export const MENU_MARKETING_SEO_AND_SEARCH = "backend.marketing.seo";
+export const MENU_MARKETING_SEO_AND_SEARCH = "marketing.seo.search";
 
 /** Menu ID for "Marketing > User Content" in the Adobe Commerce admin */
-export const MENU_MARKETING_USER_CONTENT = "backend.marketing.user.content";
+export const MENU_MARKETING_USER_CONTENT = "marketing.user.content";
 
 /** Menu ID for "Reports" in the Adobe Commerce admin */
-export const MENU_REPORTS = "reports.report";
+export const MENU_REPORTS = "reports";
 
 /** Menu ID for "Reports > Business Intelligence" in the Adobe Commerce admin */
 export const MENU_REPORTS_BUSINESS_INTELLIGENCE =
-  "analytics.business.intelligence";
+  "reports.business.intelligence";
 
 /** Menu ID for "Reports > Customers" in the Adobe Commerce admin */
-export const MENU_REPORTS_CUSTOMERS = "reports.report.customers";
+export const MENU_REPORTS_CUSTOMERS = "reports.customers";
 
 /** Menu ID for "Reports > Marketing" in the Adobe Commerce admin */
-export const MENU_REPORTS_MARKETING = "reports.report.marketing";
+export const MENU_REPORTS_MARKETING = "reports.marketing";
 
 /** Menu ID for "Reports > Private Sales" in the Adobe Commerce admin */
-export const MENU_REPORTS_PRIVATE_SALES =
-  "invitation.report.magento.invitation";
+export const MENU_REPORTS_PRIVATE_SALES = "reports.private.sales";
 
 /** Menu ID for "Reports > Products" in the Adobe Commerce admin */
-export const MENU_REPORTS_PRODUCTS = "reports.report.products";
+export const MENU_REPORTS_PRODUCTS = "reports.products";
 
 /** Menu ID for "Reports > Reviews" in the Adobe Commerce admin */
-export const MENU_REPORTS_REVIEWS = "review.report.review";
+export const MENU_REPORTS_REVIEWS = "reports.reviews";
 
 /** Menu ID for "Reports > Sales" in the Adobe Commerce admin */
-export const MENU_REPORTS_SALES = "reports.report.salesroot";
+export const MENU_REPORTS_SALES = "reports.sales";
 
 /** Menu ID for "Reports > Statistics" in the Adobe Commerce admin */
-export const MENU_REPORTS_STATISTICS = "reports.report.statistics";
+export const MENU_REPORTS_STATISTICS = "reports.statistics";
 
 /** Menu ID for "Reports > System Insights" in the Adobe Commerce admin */
-export const MENU_REPORTS_SYSTEM_INSIGHTS = "swat.system.insights";
+export const MENU_REPORTS_SYSTEM_INSIGHTS = "reports.system.insights";
 
 /** Menu ID for "Sales" in the Adobe Commerce admin */
-export const MENU_SALES = "sales.sales";
+export const MENU_SALES = "sales";
 
 /** Menu ID for "Sales > Archive" in the Adobe Commerce admin */
-export const MENU_SALES_ARCHIVE = "salesarchive.sales.archive";
+export const MENU_SALES_ARCHIVE = "sales.archive";
 
 /** Menu ID for "Sales > Operations" in the Adobe Commerce admin */
-export const MENU_SALES_OPERATIONS = "sales.sales.operation";
+export const MENU_SALES_OPERATIONS = "sales.operations";
 
 /** Menu ID for "Stores" in the Adobe Commerce admin */
-export const MENU_STORES = "backend.stores";
+export const MENU_STORES = "stores";
 
 /** Menu ID for "Stores > Attributes" in the Adobe Commerce admin */
-export const MENU_STORES_ATTRIBUTES = "backend.stores.attributes";
+export const MENU_STORES_ATTRIBUTES = "stores.attributes";
 
 /** Menu ID for "Stores > Currency" in the Adobe Commerce admin */
-export const MENU_STORES_CURRENCY = "currencysymbol.system.currency";
+export const MENU_STORES_CURRENCY = "stores.currency";
 
 /** Menu ID for "Stores > Inventory" in the Adobe Commerce admin */
-export const MENU_STORES_INVENTORY = "inventoryadminui.inventory";
+export const MENU_STORES_INVENTORY = "stores.inventory";
 
 /** Menu ID for "Stores > Other Settings" in the Adobe Commerce admin */
-export const MENU_STORES_OTHER_SETTINGS = "backend.other.settings";
+export const MENU_STORES_OTHER_SETTINGS = "stores.other.settings";
 
 /** Menu ID for "Stores > Settings" in the Adobe Commerce admin */
-export const MENU_STORES_SETTINGS = "backend.stores.settings";
+export const MENU_STORES_SETTINGS = "stores.settings";
 
 /** Menu ID for "Stores > Taxes" in the Adobe Commerce admin */
-export const MENU_STORES_TAXES = "tax.sales.tax";
+export const MENU_STORES_TAXES = "stores.taxes";
 
 /** Menu ID for "System" in the Adobe Commerce admin */
-export const MENU_SYSTEM = "backend.system";
+export const MENU_SYSTEM = "system";
 
 /** Menu ID for "System > Action Logs" in the Adobe Commerce admin */
-export const MENU_SYSTEM_ACTION_LOGS = "logging.system.magento.logging";
+export const MENU_SYSTEM_ACTION_LOGS = "system.action.logs";
 
 /** Menu ID for "System > Admin UI SDK" in the Adobe Commerce admin */
-export const MENU_SYSTEM_ADMIN_UI_SDK =
-  "commercebackenduix.adminuisdk.management";
+export const MENU_SYSTEM_ADMIN_UI_SDK = "system.admin.ui.sdk";
 
 /** Menu ID for "System > Data Transfer" in the Adobe Commerce admin */
-export const MENU_SYSTEM_DATA_TRANSFER = "backend.system.convert";
+export const MENU_SYSTEM_DATA_TRANSFER = "system.data.transfer";
 
 /** Menu ID for "System > Events" in the Adobe Commerce admin */
-export const MENU_SYSTEM_EVENTS = "adobecommerceeventsclient.menu";
+export const MENU_SYSTEM_EVENTS = "system.events";
 
 /** Menu ID for "System > Extensions" in the Adobe Commerce admin */
-export const MENU_SYSTEM_EXTENSIONS = "integration.system.extensions";
+export const MENU_SYSTEM_EXTENSIONS = "system.extensions";
 
 /** Menu ID for "System > Other Settings" in the Adobe Commerce admin */
-export const MENU_SYSTEM_OTHER_SETTINGS = "backend.system.other.settings";
+export const MENU_SYSTEM_OTHER_SETTINGS = "system.other.settings";
 
 /** Menu ID for "System > Permissions" in the Adobe Commerce admin */
-export const MENU_SYSTEM_PERMISSIONS = "user.system.acl";
+export const MENU_SYSTEM_PERMISSIONS = "system.permissions";
 
 /** Menu ID for "System > Services" in the Adobe Commerce admin */
-export const MENU_SYSTEM_SERVICES = "servicesid.configuration";
+export const MENU_SYSTEM_SERVICES = "system.services";
 
 /** Menu ID for "System > Support" in the Adobe Commerce admin */
-export const MENU_SYSTEM_SUPPORT = "support.support";
+export const MENU_SYSTEM_SUPPORT = "system.support";
 
 /** Menu ID for "System > Tools" in the Adobe Commerce admin */
-export const MENU_SYSTEM_TOOLS = "backend.system.tools";
+export const MENU_SYSTEM_TOOLS = "system.tools";
 
 /** Menu ID for "System > Webhooks" in the Adobe Commerce admin */
-export const MENU_SYSTEM_WEBHOOKS = "adobecommercewebhooks.menu";
+export const MENU_SYSTEM_WEBHOOKS = "system.webhooks";
 
 /** Menu ID for "System > Observability" in the Adobe Commerce admin */
-export const MENU_SYSTEM_OBSERVABILITY = "outofprocessobservability.menu";
+export const MENU_SYSTEM_OBSERVABILITY = "system.observability";
 
 /** All menu categories available in Adobe Commerce. */
 export const COMMERCE_MENUS = [
