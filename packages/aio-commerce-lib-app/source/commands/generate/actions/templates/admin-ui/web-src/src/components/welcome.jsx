6108cb979fb67bce13b64237fe8b7b3e4b4d12db
Ivan Porto Wigner <iporto@adobe.com>
2026-06-30 21:26:11 +0200

    refactor: add simple jsdoc comment in template

--- packages/aio-commerce-lib-app/source/commands/generate/actions/templates/admin-ui/web-src/src/components/welcome.jsx
+++ packages/aio-commerce-lib-app/source/commands/generate/actions/templates/admin-ui/web-src/src/components/welcome.jsx
@@ -1,5 +1,6 @@
 import { useIms } from "@adobe/aio-commerce-lib-admin-ui/web";
 
+/** A simple welcome component that displays the IMS token and org ID. */
 export function Welcome() {
   const { imsToken, imsOrgId } = useIms();
   return (
