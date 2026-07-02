db858c0029c47284fe00342ec87011cc6fbd6eac
Ivan Porto Wigner <iporto@adobe.com>
2026-07-02 15:24:34 +0200

    refactor: don't show ims token in default generated template

--- packages/aio-commerce-lib-app/source/commands/generate/actions/templates/admin-ui/web-src/src/components/welcome.jsx
+++ packages/aio-commerce-lib-app/source/commands/generate/actions/templates/admin-ui/web-src/src/components/welcome.jsx
@@ -2,13 +2,11 @@ import { useIms } from "@adobe/aio-commerce-lib-admin-ui/web";
 
 /** A simple welcome component that displays the IMS token and org ID. */
 export function Welcome() {
-  const { imsToken, imsOrgId } = useIms();
+  const { imsOrgId } = useIms();
   return (
     <>
       <h1>Welcome to your Adobe Commerce App</h1>
-
-      <p>IMS Token: {`${imsToken.slice(0, 10)}...`}</p>
-      <p>IMS Org ID: {imsOrgId}</p>
+      <p>Your IMS Org ID is {imsOrgId}</p>
     </>
   );
 }
