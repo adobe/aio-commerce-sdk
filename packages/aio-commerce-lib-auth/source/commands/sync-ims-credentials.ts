3bb779dd8ee3bf9ad5204f268bb7aa6ed564c1fe
Ivan Porto Wigner <iporto@adobe.com>
2026-08-19 16:22:20 +0200

    refactor: project root dep in new main code

--- packages/aio-commerce-lib-auth/source/commands/sync-ims-credentials.ts
+++ packages/aio-commerce-lib-auth/source/commands/sync-ims-credentials.ts
@@ -1,5 +1,18 @@
+/*
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 import { syncImsCredentials } from "@aio-commerce-sdk/scripting-utils/env";
 import { stringifyError } from "@aio-commerce-sdk/scripting-utils/error";
+import { getProjectRootDirectory } from "@aio-commerce-sdk/scripting-utils/project";
 import consola from "consola";
 
 /** Synchronizes the current context IMS credentials to their AIO_COMMERCE_IMS_AUTH counterparts. */
@@ -7,7 +20,8 @@ export async function run() {
   consola.start("Syncing IMS credentials...");
 
   try {
-    const result = await syncImsCredentials();
+    const projectRoot = await getProjectRootDirectory();
+    const result = await syncImsCredentials(projectRoot);
 
     if (!result.ok) {
       switch (result.reason) {
